69d573eb508f5412ba2ee18cc662d75cb777a0a1
onk <oleg.kyiashko@gmail.com>
2012-05-06 00:03:56 +0300

    fix

--- flashdump.sh
+++ flashdump.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 #set -vx
-BASEDIR=`basedir $0`
+BASEDIR=`dirname $0`
 PATH=$BASEDIR/bin:$PATH
 export PATH
 
 echo Check that your tablet is in the firmware flash mode and connected to computer
 
+mkdir flashdump
+cd flashdump
+
 sudo rkflashtool r 0 1 >parm
 mkkrnlimg -r parm parameter
+
 PARAMFILE="parameter"
 
-MODEL=`grep MACHINE_MODEL ${PARAMFILE}|cut -d: -f2|tr -d "\n\r"`
 PARTS=`grep CMDLINE ${PARAMFILE}|cut -d: -f3|tr -d "\n\r"`
 
 PARTS=(${PARTS//,/ })
 
 REGEX1="(0x[0-9a-fA-F]*)@(0x[0-9a-fA-F]*)\(([a-z]*)\)"
 
-mkdir flashdump
 for PART in "${PARTS[@]}"
 do
 	if [[ "${PART}" =~ ${REGEX1} ]]
@@ -27,7 +29,7 @@ do
 		sname=${BASH_REMATCH[3]}
 
 		cmd=`printf "rkflashtool r 0x%08x 0x%08x " ${sstart} ${ssize}`
-		sudo $cmd > flashdump/${sname}
+		sudo $cmd > ${sname}
 	fi
 done
 
