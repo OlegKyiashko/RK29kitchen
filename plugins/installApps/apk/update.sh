802e3653c4f5b69e2bae20c6f7b8e8ffc67d1fcb
onk <oleg.kyiashko@gmail.com>
2012-07-03 08:34:30 +0300

    up

--- plugins/installApps/apk/update.sh
+++ plugins/installApps/apk/update.sh
@@ -4,4 +4,5 @@ rm tcandroid*.apk
 wget http://ghisler.com/tcandroid.apk
 rm GO_Launcher_HD.apk
 wget http://61.145.124.93/soft/3GHeart/golauncher/pad/GO_Launcher_HD.apk
-
+rm MarketEnabler*apk
+wget http://market-enabler.googlecode.com/files/MarketEnabler_v3113.apk
