9e2934f688544694163180c9a8ff41ddf81afce8
onk <oleg.kyiashko@gmail.com>
2012-10-25 12:12:50 +0300

    ubuntu compatible version (and some other linux/bsd)

--- bin/png2clut224.sh
+++ bin/png2clut224.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -vx
 BASEDIR=`dirname $0`
 pushd $BASEDIR >/dev/null
 BASEDIR=$(pwd)
 popd >/dev/null
 PATH=$BASEDIR:$PATH
 
-pngtopnm -plain $1 | ppmquant -plain 224  > $1.ppm
+#pngtopnm -plain $1 | ppmquant -plain 224  > $1.ppm
+pngtopnm  $1 | ppmquant  224 | pnmtoplainpnm > $1.ppm
+
 pnm2clut224 $1.ppm
